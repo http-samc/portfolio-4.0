616d4d0327124782d4dc4d8c930ca303865291c8
http-samc <67826352+http-samc@users.noreply.github.com>
2022-03-26 22:44:30 -0500

    Update [...slug].ts

--- pages/api/[...slug].ts
+++ pages/api/[...slug].ts
@@ -4,7 +4,7 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { slug }: any = req.query
   try {
-    res.redirect(307, `https://api.smrth.dev/api/${slug.join('/')}`)
+    res.redirect(307, `https://legacy.smrth.dev/api/${slug.join('/')}`)
   } catch (err) {
     res.status(500).send({ error: 'Error! API servers down.' })
   }
