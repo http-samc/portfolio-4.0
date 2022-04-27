d939ed58be4209451e7dd5d3cfb96cc731f36d7f
http-samc <67826352+http-samc@users.noreply.github.com>
2022-04-27 10:22:48 -0500

    feat: added filesystem mapping

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/fs/:doc*',
+        destination: 'https://fs.smrth.dev/smrth/global/:doc*',
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
